262acbf9a45b39b590b6fa749bb8860a9c571337
Patrick Menendez <patrickmenendez29@gmail.com>
2020-04-14 21:27:22 -0500

    final version which works

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main (String[] args){
         int[] num = {5,4,3,2,1};
 
-        System.out.println(Shell.sort(num));
+        System.out.println(Arrays.toString(Shell.sort(num)));
     }
 }
--- src/Shell.java
+++ src/Shell.java
@@ -2,54 +2,52 @@ import java.util.ArrayList;
 
 public class Shell {
 
-    public static ArrayList<Integer> sort(int[] arr)
+    public static int[] sort(int[] arr)
     {
-        ArrayList<Integer> result = new ArrayList<>();
 
-        int gap = result.size() / 2;
+        shellSort(arr);
 
-        while (gap > 1){
+        return insertionSort(arr);
 
-            shellSort(gap, arr);
+    }
 
-            gap--;
+
+    private static void shellSort(int[] arr){
+
+        int gap = arr.length / 2;
+
+        for (int i = 0; i < arr.length - 1; i++){
+
+            if (arr[i] > arr[gap]){
+                int temp = arr[i];
+                arr[i] = arr[gap];
+                arr[gap] = temp;
+
+            }
+
         }
 
-        insertionSort(arr);
-
-
-        return result;
-
     }
 
 
-    private static void shellSort(int gap, int[] arr){
-
-        
-    }
-
-
-    private static void swap(Integer a, Integer b){
-        Integer temp = a;
-        a = b;
-        b = temp;
-    }
-
-    private static void insertionSort(int arr[])
+    private static int[] insertionSort(int[] arr)
     {
+
         int n = arr.length;
         for (int i = 1; i < n; ++i) {
             int key = arr[i];
             int j = i - 1;
 
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
+
             }
+
             arr[j + 1] = key;
         }
+
+        return arr;
+
     }
 }
